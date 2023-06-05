b48be0a60f09f5f9e4614ce81159d9774c49a4cf
BaukeWestendorp <baukewestendorp@gmail.com>
2023-06-06 00:06:48 +0200

    Add layout lib and sputnik executable

---
+++ sputnik/src/main.rs
@@ -0,0 +1,25 @@
+use parser::Parser;
+use std::time::Instant;
+use typed_arena::Arena;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: sputnik <path-to-html-file>");
+        std::process::exit(1);
+    }
+    let path = args[1].clone();
+
+    let arena = Arena::new();
+
+    let html = std::fs::read_to_string(path).unwrap();
+    let parser = Parser::new(arena, html.as_str());
+
+    let before = Instant::now();
+    let document = parser.parse();
+    let after = Instant::now();
+    let time = after.duration_since(before);
+
+    document.dump(Default::default());
+    eprintln!("Took {:?} to parse document!", time);
+}
